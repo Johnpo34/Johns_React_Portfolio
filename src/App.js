74eef27d1e20d7721801a5918abf10782b2e1fb7
John Pohl <johnpohl34@gmail.com>
2022-05-03 00:17:37 -0400

    header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import Aboutme from "./components/Aboutme";
 import Header from "./components/Header";
 import PortfolioContainer from "./components/PortfolioContainer";
 import Footer from "./components/Footer";
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="app">
       <Header/>
+      <Aboutme/>
       <PortfolioContainer/>
       <Footer/>
     </div>
